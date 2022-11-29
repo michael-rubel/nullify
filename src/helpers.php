59ca5574f6fc9e1e1e018446075225f373a02c8d
Michael Rubél <contact@observer.name>
2022-11-29 22:46:33 +0100

    Add return type 🧫

--- src/helpers.php
+++ src/helpers.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use MichaelRubel\Nullify\Nullify;
 
 if (! function_exists('nullify')) {
-    function nullify(mixed $values)
+    function nullify(mixed $values): mixed
     {
         return (new Nullify)($values);
     }
